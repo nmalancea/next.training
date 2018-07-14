216443195ae3f0247256dc6873e155522828b5c9
Nelu Malancea <nelum@extrahop.com>
2018-07-14 08:24:18 -0700

    Prepare for deployment to netlify

--- next.config.js
+++ next.config.js
@@ -2,11 +2,48 @@ require('@remy/envy')
 const webpack = require('webpack')
 const withCss = require('@zeit/next-css')
 
-module.exports = withCss({
-  cssModules: false,
-  webpack: config => {
-    config.plugins.push(new webpack.EnvironmentPlugin(['SHOW_SPEAKER']))
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
 
-    return config
-  }
-})
+module.exports = withCss(
+  withBundleAnalyzer({
+    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    bundleAnalyzerConfig: {
+      server: {
+        analyzerMode: 'static',
+        reportFilename: '../../bundles/server.html'
+      },
+      browser: {
+        analyzerMode: 'static',
+        reportFilename: '../bundles/client.html'
+      },
+      openAnalyzer: true
+    },
+    cssModules: false,
+    webpack: config => {
+      config.plugins.push(new webpack.EnvironmentPlugin(['SHOW_SPEAKER']))
+
+      return config
+    },
+    exportPathMap: async function(defaultPathMap) {
+      return {
+        '/': { page: '/' },
+        '/about': { page: '/about' },
+        '/contact': { page: '/contact' },
+        '/session/memory': { page: '/session', query: { slug: 'memory' } },
+        '/session/rethinking': {
+          page: '/session',
+          query: { slug: 'rethinking' }
+        },
+        '/session/passwords': {
+          page: '/session',
+          query: { slug: 'passwords' }
+        },
+        '/session/art': {
+          page: '/session',
+          query: { slug: 'art' }
+        }
+      }
+    }
+  })
+)
